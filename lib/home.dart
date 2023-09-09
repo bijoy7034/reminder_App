31e3500ce88a68a7b656def58d7bd85b3e1f62c8
Bijoy Anil <86096110+bijoy7034@users.noreply.github.com>
2023-09-09 23:54:17 +0530

    2

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,11 @@
-import 'package:day_night_time_picker/day_night_time_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+
+import 'main.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -10,126 +13,175 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String selectedValue = 'Wake up';
-  Time _time = Time(hour: 12, minute: 0);
-  bool iosStyle = true;
-  void onTimeChanged(Time newTime) {
-    setState(() {
-      _time = newTime;
-    });
+  TimeOfDay _time = TimeOfDay(hour: 12, minute: 0);
+
+  Future<void> _scheduleNotification() async {
+    final AndroidNotificationDetails androidPlatformChannelSpecifics =
+    AndroidNotificationDetails(
+      'your_channel_id',
+      'your_channel_name',
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+    );
+
+    final NotificationDetails platformChannelSpecifics =
+    NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      0,
+      'Reminder',
+      'It\'s time for $selectedValue',
+      _nextInstanceOfSelectedTime(),
+      platformChannelSpecifics,
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+      UILocalNotificationDateInterpretation.absoluteTime,
+      payload: 'custom_notification',
+    );
   }
+
+  tz.TZDateTime _nextInstanceOfSelectedTime() {
+    final now = tz.TZDateTime.now(tz.local);
+    return tz.TZDateTime(
+      tz.local,
+      now.year,
+      now.month,
+      now.day,
+      _time.hour,
+      _time.minute,
+    ).add(const Duration(seconds: 1)); // Adding 1 second to avoid scheduling in the past
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: _time,
+    );
+
+    if (pickedTime != null && pickedTime != _time) {
+      setState(() {
+        _time = pickedTime;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.amber,
-          title: Text('Reminder App', style: TextStyle(color: Colors.white),),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.amber,
+        title: Text(
+          'Reminder App',
+          style: TextStyle(color: Colors.white),
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(left: 28.0, right: 28),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height:30),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('Set reminders for daily activities', style: TextStyle(color: Colors.grey, fontSize: 16),),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 28.0, right: 28),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 10),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Set reminders for daily activities',
+                  style: TextStyle(color: Colors.grey, fontSize: 16),
                 ),
               ),
-              Container(
-                width: double.infinity,
-                height: 37,
-                padding: EdgeInsets.all(12),
-                decoration: BoxDecoration(
-                  color: Colors.amber,
-                ),
-                child: Center(
-                  child: DropdownButton<String>(
-                    dropdownColor: Colors.grey.shade900,
-                    underline: SizedBox(),
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                    value: selectedValue,
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        selectedValue = newValue!;
-                      });
-                    },
-                    items: <String>[
-                      'Wake up',
-                      'Go to gym',
-                      'Breakfast',
-                      'Meetings',
-                      'Lunch',
-                      'Quick nap',
-                      'Go to library',
-                      'Dinner',
-                      'Go to sleep',
-                    ].map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value, textAlign:TextAlign.center,),
-                      );
-                    }).toList(),
-                  ),
-                ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 37,
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.amber,
               ),
-              SizedBox(height: 10,),
-              Container(
-                width: double.infinity,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.secondary,
+              child: Center(
+                child: DropdownButton<String>(
+                  dropdownColor: Colors.grey.shade900,
+                  underline: SizedBox(),
+                  style: TextStyle(
+                    color: Colors.white,
                   ),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      showPicker(
-                        showSecondSelector: true,
-                        context: context,
-                        value: _time,
-                        onChange: onTimeChanged,
-                        minuteInterval: TimePickerInterval.FIVE,
-                        // Optional onChange to receive value as DateTime
-                        onChangeDateTime: (DateTime dateTime) {
-                          // print(dateTime);
-                          debugPrint("[debug datetime]:  $dateTime");
-                        },
+                  value: selectedValue,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedValue = newValue!;
+                    });
+                  },
+                  items: <String>[
+                    'Wake up',
+                    'Go to gym',
+                    'Breakfast',
+                    'Meetings',
+                    'Lunch',
+                    'Quick nap',
+                    'Go to library',
+                    'Dinner',
+                    'Go to sleep',
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        textAlign: TextAlign.center,
                       ),
                     );
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: const Text(
-                      "Select the time",
-                      style: TextStyle(color: Colors.white),
-                    ),
+                  }).toList(),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.secondary,
+                ),
+                onPressed: () {
+                  _selectTime(context);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Text(
+                    "Select the time",
+                    style: TextStyle(color: Colors.white),
                   ),
                 ),
               ),
-              SizedBox(height: 10,),
-              Text('Selected Activity :  ${selectedValue}', style: TextStyle(color: Colors.white),),
-              SizedBox(height: 10,),
-              Text('Selected Time :  ${_time.format(context)}', style: TextStyle(color: Colors.white),),
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Container(
-                  width: double.infinity,
-                  child: ElevatedButton(onPressed: (){
-
-                  }, child: Text(
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Selected Activity :  ${selectedValue}',
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Selected Time :  ${_time.format(context)}',
+              style: TextStyle(color: Colors.white),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: _scheduleNotification,
+                  child: Text(
                     "Set Reminder",
                     style: TextStyle(color: Colors.white),
-                  ),),
+                  ),
                 ),
-              )
-            ],
-          ),
-        )
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
